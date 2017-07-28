ee36e3d170aa115f038f048cf7e62cd148fdc62d
Onlymo <X-5m@live.com>
2017-07-28 10:38:09 -0400

    Update gulpfile.js

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -6,7 +6,7 @@ var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
 
 gulp.task('css', function () {
-	return gulp.src('taufix/css/*style.css')
+	return gulp.src('MUHAMAD/css/*style.css')
 	.pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}, {errLogToConsole: true}))
 	.pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
 	.pipe(gulp.dest('taufix/css'))
@@ -23,8 +23,8 @@ gulp.task('browser-sync', ['nodemon'], function() {
 });
 
 gulp.task('default', ['css', 'browser-sync'], function () {
-	gulp.watch("taufix/css/*style.css", ['css']);
-	gulp.watch(["taufix/js/**/*.js", "./*.html"], reload);
+	gulp.watch("MUHAMAD/css/*style.css", ['css']);
+	gulp.watch(["MUHAMAD/js/**/*.js", "./*.html"], reload);
 });
 
 gulp.task('nodemon', function (cb) {
